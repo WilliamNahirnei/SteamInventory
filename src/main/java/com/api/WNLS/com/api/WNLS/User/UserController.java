1fbc3c02e8469fb9fec04e80898007b183bcf9b0
William_Kolina <william_kolina@pop-os.localdomain>
2022-09-28 13:29:21 -0300

    Create put route and geters routes of user

--- src/main/java/com/api/WNLS/com/api/WNLS/User/UserController.java
+++ src/main/java/com/api/WNLS/com/api/WNLS/User/UserController.java
@@ -1,11 +1,16 @@
 package com.api.WNLS.com.api.WNLS.User;
 
 import com.api.WNLS.com.api.WNLS.Utils.Exceptions.ValidationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.UUID;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -21,6 +26,38 @@ public class UserController {
     public ResponseEntity<Object> saveUser(@RequestBody @Valid UserDTO userDTO) {
         try{
             return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(userDTO));
+        }
+        catch (ValidationException e){
+            return ResponseEntity.status(e.getHttpCode()).body(e.getValidationMessage());
+        }
+        catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal server error");
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<UserModel>> getAllParkingSpots(@PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.ASC) Pageable pageable){
+        return ResponseEntity.status(HttpStatus.OK).body(userService.findAll(pageable));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getParkingSpotById(@PathVariable(value = "id") UUID id){
+        try{
+            return ResponseEntity.status(HttpStatus.OK).body(userService.findById(id));
+        }
+        catch (ValidationException e){
+            return ResponseEntity.status(e.getHttpCode()).body(e.getValidationMessage());
+        }
+        catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal server error");
+        }
+    }
+
+    @PutMapping("/{id}")
+    ResponseEntity<Object> updateUser(@PathVariable(value = "id") UUID id,
+                                             @RequestBody @Valid UserDTO userDTO){
+        try{
+            return ResponseEntity.status(HttpStatus.OK).body(userService.upddate(id, userDTO));
         }
         catch (ValidationException e){
             return ResponseEntity.status(e.getHttpCode()).body(e.getValidationMessage());
--- src/main/java/com/api/WNLS/com/api/WNLS/User/UserService.java
+++ src/main/java/com/api/WNLS/com/api/WNLS/User/UserService.java
@@ -1,9 +1,15 @@
 package com.api.WNLS.com.api.WNLS.User;
 
+import com.api.WNLS.com.api.WNLS.Utils.Exceptions.ValidationException;
 import org.springframework.beans.BeanUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class UserService {
@@ -17,7 +23,51 @@ public class UserService {
     public UserModel save (UserDTO userDTO) throws Exception {
         var userModel = new UserModel();
         BeanUtils.copyProperties(userDTO, userModel);
-//        userModel.setUser_type(UserType.USER);
+        userModel.setUser_type(UserType.USER);
         return userRepository.save(userModel);
+    }
+//    private void validateUniqueAttributes(ParkingSpotDTO parkingSpotDTO) throws ValidationException {
+//        if (this.existsByLicensePlateCar(parkingSpotDTO.getLicensePlateCar()))
+//            throw new ValidationException("Conflict: License plate car is alert in use", 409, "Conflict: License plate car is alert in use");
+//        if (this.existsByParkingSpotNumber(parkingSpotDTO.getParkingSpotNumber()))
+//            throw new ValidationException("Conflict: Parking spot is already in use", 409, "Conflict: Parking spot is already in use");
+//        if (this.existsByApartmentAndBlock(parkingSpotDTO.getApartment(), parkingSpotDTO.getBlock()))
+//            throw new ValidationException("Conflict: Parking spot already registered in this apartment and block", 409, "Conflict: Parking spot already registered in this apartment and block");
+//    }
+
+//    public  boolean existsByLicensePlateCar(String licensePlateCar) {
+//        return parkingSpotRepository.existsByLicensePlateCar(licensePlateCar);
+//    }
+//
+//    public boolean existsByParkingSpotNumber(String parkingSpotNumber) {
+//        return parkingSpotRepository.existsByParkingSpotNumber(parkingSpotNumber);
+//    }
+//
+//    public boolean existsByApartmentAndBlock(String apartment, String block) {
+//        return parkingSpotRepository.existsByApartmentAndBlock(apartment, block);
+//    }
+
+    public Page<UserModel> findAll(Pageable pageable) {
+        return userRepository.findAll(pageable);
+    }
+
+    public UserModel findById(UUID id) throws ValidationException {
+        Optional<UserModel> userOptional = userRepository.findById(id);
+        if (!userOptional.isPresent())
+            throw new ValidationException("User not found", HttpStatus.NOT_FOUND.value(), "User not Found");
+        return userOptional.get();
+    }
+
+    @Transactional
+    public UserModel upddate(UUID id, UserDTO userDTO) throws ValidationException {
+        UserModel user = this.findById(id);
+        BeanUtils.copyProperties(userDTO, user);
+        return userRepository.save(user);
+    }
+
+    @Transactional
+    public void delete(UUID id) throws ValidationException {
+        UserModel user = this.findById(id);
+        userRepository.delete(user);
     }
 }
