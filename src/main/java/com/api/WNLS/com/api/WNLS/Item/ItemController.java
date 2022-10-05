00fa22c032b082b4b6eabaf68ad9e9ee15b37f6f
William_Kolina <william_kolina@pop-os.localdomain>
2022-10-05 14:01:05 -0300

    Add route to save new item, get all items with pagination, and get item by id

---
+++ src/main/java/com/api/WNLS/com/api/WNLS/Item/ItemController.java
@@ -0,0 +1,54 @@
+package com.api.WNLS.com.api.WNLS.Item;
+
+import com.api.WNLS.com.api.WNLS.User.UserModel;
+import com.api.WNLS.com.api.WNLS.Utils.Exceptions.ValidationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.UUID;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/items")
+public class ItemController {
+
+    final ItemService itemService;
+
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    @PostMapping()
+    public ResponseEntity<Object> saveItem(@RequestBody @Valid ItemDTO itemDTO) {
+        try {
+            return ResponseEntity.status(HttpStatus.CREATED).body(itemService.save(itemDTO));
+        }
+        catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal server error");
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ItemModel>> getAllItems(@PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.ASC) Pageable pageable){
+        return ResponseEntity.status(HttpStatus.OK).body(itemService.findAll(pageable));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getItemById(@PathVariable(value = "id") UUID id){
+        try{
+            return ResponseEntity.status(HttpStatus.OK).body(itemService.findById(id));
+        }
+        catch (ValidationException e){
+            return ResponseEntity.status(e.getHttpCode()).body(e.getValidationMessage());
+        }
+        catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal server error");
+        }
+    }
+}
